39c11e5283d2422903e1eb32d042cdc340e51e79
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2022-11-09 03:43:22 +0000

    12-2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // （API_KEY には、"取得したAPIキー" を記述）
-const API_KEY = "取得したAPIキー";
+const API_KEY = "e5e7c579498f448ab8ba33f93ecfc37";
 $(function(){
   $('#btn').on('click', function() {
     // 入力された都市名でWebAPIに天気情報をリクエスト
     $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + "e5e7c579498f448ab8ba33f93ecfc37",
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
       dataType : 'jsonp',
     }).done(function (data){
       //通信成功
